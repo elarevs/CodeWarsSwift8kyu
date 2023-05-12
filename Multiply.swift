6f6e79f53dc36e75a44ddc77d7618df8492d2673
elarevs <108342467+elarevs@users.noreply.github.com>
2023-05-13 00:53:21 +0200

    Update and rename Multiply to Multiply.swift

---
+++ Multiply.swift
@@ -0,0 +1,11 @@
+// This code does not execute properly. Try to figure out why.
+
+// func multiply(_ a: Double, _ b: Double) -> Double {
+//     a / b
+// }
+
+// SOLUTION:
+
+func multiply(_ a: Double, _ b: Double) -> Double {
+    return a * b
+}
