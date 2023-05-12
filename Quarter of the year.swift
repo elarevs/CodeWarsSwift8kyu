70991111ecc8f7dba5132c6489cdef4fd1a5885a
elarevs <108342467+elarevs@users.noreply.github.com>
2023-05-13 00:57:21 +0200

    Update Quarter of the year.swift

--- Quarter of the year.swift
+++ Quarter of the year.swift
@@ -11,9 +11,9 @@
 
 func quarter(of month: Int) -> Int {
   switch month {
-    case 0...3 : return 1
-    case 4...6 : return 2
-    case 7...9 : return 3
-    default : return 4
+  case 0...3 : return 1
+  case 4...6 : return 2
+  case 7...9 : return 3
+  default : return 4
   }
 }
