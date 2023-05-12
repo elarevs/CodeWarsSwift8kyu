81ff4dcfd73d0a4b83a0403e093b168aa97ea94d
elarevs <108342467+elarevs@users.noreply.github.com>
2023-05-13 00:50:18 +0200

    Update and rename Calculate BMI to Calculate BMI.swift

--- Calculate BMI.swift
+++ Calculate BMI.swift
@@ -1,15 +1,11 @@
-Write function bmi that calculates body mass index (bmi = weight / height2).
+// Write function bmi that calculates body mass index (bmi = weight / height2).
 
-if bmi <= 18.5 return "Underweight"
+// if bmi <= 18.5 return "Underweight"
+// if bmi <= 25.0 return "Normal"
+// if bmi <= 30.0 return "Overweight"
+// if bmi > 30 return "Obese"
 
-if bmi <= 25.0 return "Normal"
-
-if bmi <= 30.0 return "Overweight"
-
-if bmi > 30 return "Obese"
-
-
-SOLUTION: 
+// SOLUTION: 
 
 func bmi(_ weight: Int, _ height: Double) -> String {
     let bmi = Double(weight) / (height * height)
