10ead078ca5fdf296a83b719cfc158a328bb5cf1
elarevs <108342467+elarevs@users.noreply.github.com>
2023-05-13 00:56:12 +0200

    Update Calculate BMI.swift

--- Calculate BMI.swift
+++ Calculate BMI.swift
@@ -10,9 +10,9 @@
 func bmi(_ weight: Int, _ height: Double) -> String {
     let bmi = Double(weight) / (height * height)
     switch bmi {
-      case 0...18.5 : return "Underweight"
-      case 18.5...25 : return "Normal"
-      case 25...30 : return "Overweight"
-      default : return "Obese"
+    case 0...18.5 : return "Underweight"
+    case 18.5...25 : return "Normal"
+    case 25...30 : return "Overweight"
+    default : return "Obese"
     }
 }
