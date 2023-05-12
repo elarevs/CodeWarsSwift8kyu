96bc8217d244ea228adf4cb56f7048d871e0275c
elarevs <108342467+elarevs@users.noreply.github.com>
2023-05-13 00:32:51 +0200

    Update Beginner - Reduce but Grow.swift

--- Beginner - Reduce but Grow.swift
+++ Beginner - Reduce but Grow.swift
@@ -1,14 +1,14 @@
-Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 
-[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 
-SOLUTION 1:
+// SOLUTION 1:
 
 func grow(_ arr: [Int]) -> Int {
   return arr.reduce(1,*)
 }
 
-SOLUTION 2:
+// SOLUTION 2:
 
 func grow(_ arr: [Int]) -> Int {
   var result = 1
