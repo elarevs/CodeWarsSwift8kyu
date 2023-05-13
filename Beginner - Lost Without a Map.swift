2992848d09496c4fc4cb5d43a5f3d3210b631e2e
Artem Elchev <108342467+elarevs@users.noreply.github.com>
2023-05-13 10:25:27 +0200

    Create Beginner - Lost Without a Map.swift

---
+++ Beginner - Lost Without a Map.swift
@@ -0,0 +1,11 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// SOLUTION:
+
+func check<T: Equatable>(_ a: [T], _ x: T) -> Bool {
+    a.contains(x) ? true : false
+}
