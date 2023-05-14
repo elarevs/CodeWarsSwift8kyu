5807ab3068084884a6d849dafb67a7d60dc2c612
Artem Elchev <108342467+elarevs@users.noreply.github.com>
2023-05-14 19:45:36 +0200

    Update Grasshopper - Grade book.swift

--- Grasshopper - Grade book.swift
+++ Grasshopper - Grade book.swift
@@ -15,15 +15,15 @@
 func getGrade(_ s1: Int, _ s2: Int, _ s3: Int) -> String {
   let score = (s1 + s2 + s3) / 3
   switch score {
-    case 0..<60:
-      return "F"
-    case 60..<70:
-      return "D"
-    case 70..<80:
-      return "C"
-    case 80..<90:
-      return "B"
-    default: 
-      return "A"
+  case 0..<60:
+    return "F"
+  case 60..<70:
+    return "D"
+  case 70..<80:
+    return "C"
+  case 80..<90:
+    return "B"
+  default: 
+    return "A"
   }
 }
