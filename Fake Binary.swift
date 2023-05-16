eef90b65b741405e0eecbb0b88b49ce92f7de93f
Artem Elchev <108342467+elarevs@users.noreply.github.com>
2023-05-16 17:18:42 +0200

    Create Fake Binary.swift

---
+++ Fake Binary.swift
@@ -0,0 +1,10 @@
+// Given a string of digits, you should replace any digit below 5 with '0' 
+// and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+// SOLUTION:
+
+func fakeBin(digits: String) -> String {
+  return String(digits.map { Int(String($0))! > 4 ? "1" : "0" } )
+}
